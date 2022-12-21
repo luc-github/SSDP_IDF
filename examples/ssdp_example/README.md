c506b949c6199eef5a383f7a2cd3459fbe5749a8
Luc <8822552+luc-github@users.noreply.github.com>
2022-12-21 09:41:38 +0800

    Code base for component and example

---
+++ examples/ssdp_example/README.md
@@ -0,0 +1,17 @@
+| Supported Targets | ESP32 | ESP32-C2 | ESP32-C3 | ESP32-S2 | ESP32-S3 |
+| ----------------- | ----- | -------- | -------- | -------- | -------- |
+
+# SSDP Example
+
+This example shows how to use the SSDP component.
+
+## Behaviour
+
+
+## Configuration
+
+
+## Implementation Details
+
+
+
---
+++ examples/ssdp_example/main/ssdp_example.c
@@ -0,0 +1,46 @@
+/* SSDP Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+
+
+
+static void ssdp_example_task(void *pvParameters)
+{
+    while (1) {
+        //TODO
+    }
+
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(&ssdp_example_task, "ssdp_task", 4096, NULL, 5, NULL);
+}
